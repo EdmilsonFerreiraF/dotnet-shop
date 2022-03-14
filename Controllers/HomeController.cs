232017129fa47b7ebdebc4cab5f865107b6e8ca1
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-03-14 16:03:16 -0300

    🚀 :rocket: [Add] Home controller - route auth
    Anonymous, authenticated, employee and manager authentication routes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -31,5 +31,25 @@ namespace Shop.Controllers
                 token = token
             };
         }
+
+        [HttpGet]
+        [Route("anonymous")]
+        [AllowAnonymous]
+        public string Anonymous() => "Anônimo";
+
+        [HttpGet]
+        [Route("authenticated")]
+        [Authorize]
+        public string Authenticated() => String.Format("Autenticado - {0}", User.Identity.Name);
+
+        [HttpGet]
+        [Route("employee")]
+        [Authorize(Roles = "employee,manager")]
+        public string Employee() => "Funcionário";
+
+        [HttpGet]
+        [Route("manager")]
+        [Authorize(Roles = "manager")]
+        public string Manager() => "Gerente";
     }
 }
